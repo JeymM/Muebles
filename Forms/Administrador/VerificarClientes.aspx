710d7690196a897084173e10e5d9b25f31519f1f
David <davidfg102014@gmail.com>
2021-09-29 00:06:29 -0500

    cambios en formulario Verificar clientes

--- Forms/Administrador/VerificarClientes.aspx
+++ Forms/Administrador/VerificarClientes.aspx
@@ -11,7 +11,7 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="card">
-					<div class="card-header bg-info text-white">Productos</div>
+					<div class="card-header bg-info text-white" id="tab">Productos</div>
 					<div class="card-body">
 
 						<div class="form-group">
@@ -20,12 +20,9 @@
 						<table class="table table-striped table-hover">
 							<thead>
 								<tr>
-									<th scope="col">ID Producto</th>
-									<th scope="col">Cantidad</th>
-									<th scope="col">Precio</th>
-									<th scope="col">Descripción</th>
+									<th scope="col">ID Cliente</th>
 									<th scope="col">Nombre</th>
-									<th scope="col">Actualizar Stock</th>
+									<th scope="col">Correo</th>
 								</tr>
 							</thead>
 							<tbody>
--- Forms/Administrador/VerificarClientes.aspx.cs
+++ Forms/Administrador/VerificarClientes.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Muebles.DTO;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,40 @@ namespace Muebles.Forms.Administrador
 {
     public partial class VerificarClientes : System.Web.UI.Page
     {
+        public static DTO.ClienteDTO cl = new DTO.ClienteDTO();
+        DataTable tabla = new DataTable();
+        private object tab;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            tabla.Columns.Add("Id cliente");
+            tabla.Columns.Add("Nombre");
+            tabla.Columns.Add("Correo");
+        }
+        public void listarCliente()
+        {
+            DTO.ClienteDTO cli = new DTO.ClienteDTO();
+            cli.consultarTodos();
+
+            String tabla = "<table border=1>";
+            String[] col = { "Id", "Nombre", "Correo"};
+            tabla = tabla + "<tr>";
+            for (int i = 0; i < 3; i++)
+            {
+                tabla = tabla + "<th>" + col[i] + "</th>";
+            }
+            tabla = tabla + "</tr>";
+            foreach (ClienteDTO dat in cli.consultarTodos())
+            {
+                tabla = tabla + "<tr>";
+                tabla = tabla + "<td>" + cli.id + "</td>";
+                tabla = tabla + "<td>" + cli.nomb + "</td>";
+                tabla = tabla + "<td>" + cli.correo + "</td>";
+                tabla = tabla + "</tr>";
+            }
+            tabla = tabla + "</table>";
+            tab = tab + tabla;
+            
 
         }
     }
